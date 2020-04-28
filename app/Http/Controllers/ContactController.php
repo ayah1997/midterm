143dfa7a5d572e0f6c70926996f02138a2525413
ayakhader <sayedahmmeda@gmail.com>
2020-04-28 06:17:12 +0300

    midterm

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index(){
+       // $a=1;
+       // $contactس = Contact::orderBy('id')->get();
+
+       $contacts = Contact::all();
+        return view('contact.index',compact('contacts'));
+    }
+    public function create(){
+       
+         return view('contact.create');
+     }
+     public function store(Request $request){
+        $request->validate([
+        'username'=>'required',
+        'useremail'=>'required',
+        'userphone'=>'required',
+        ]);
+        $contact= new Contact();
+        $contact->name=$request->username;
+        $contact->email=$request->useremail;
+        $contact->phone=$request->userphone;
+        $contact->save();
+       //  $a=a+1;
+        return redirect()->back();
+    }
+    public function edit(Contact $contact){
+       // $contact = Contact::findOrFail($id);
+        return view('contact.create',compact('contact'));
+    }
+    public function update(Request $request,Contact $contact){
+        $request->validate([
+            'username'=>'required',
+            'useremail'=>'required',
+            'userphone'=>'required',
+            ]);
+           // $contact= new Contact();
+      //  $contact = Contact::findOrFail($id);
+
+            $contact->name=$request->username;
+            $contact->email=$request->useremail;
+            $contact->phone=$request->userphone;
+            $contact->save();
+             return redirect('/contact');
+
+
+    }
+    public function destroy ($id){
+
+        //  DB::table('contacts')->where('id','=',$id)->delete();
+  
+         $contact=Contact::find($id);
+        $contact->delete($id);
+          return redirect('/contact');
+          
+      }
+}
